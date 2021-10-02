aebd5ece8c0439b0fe3fb64d14335e6b81fe4f50
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-10-02 13:08:03 -0300

    Lendo tarefas do firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import { firebase } from "./firebase";
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const db = firebase.firestore();
+        const { docs } = await db.collection("tasks").get();
+
+        const arrayData = docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setTasks(arrayData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div className="container">
-      <h1>teste</h1>
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-6">
+          <ul className="list-group">
+            {tasks.map((item) => (
+              <li className="list-group-item" key={item.id}>
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-md-6">Formulário</div>
+      </div>
     </div>
   );
 }
