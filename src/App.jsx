fe0a3a0fcdaed9ebdf1ecf61367be2483d8ccddd
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-10-02 12:41:56 -0300

    Adicionando firebase no projeto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>teste</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA__ACPuuHAL-hClucKH-RG20MjTHR1Wzw",
+  authDomain: "crud-react-firebase-todo-list.firebaseapp.com",
+  projectId: "crud-react-firebase-todo-list",
+  storageBucket: "crud-react-firebase-todo-list.appspot.com",
+  messagingSenderId: "54283305390",
+  appId: "1:54283305390:web:aa4206678a08aa9ec4883f",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
